a86b0886db0fda5c38547b135b9bda1ae83c58dc
andjelagajic <andjela.gajic.4224@gmail.com>
2022-01-25 08:27:22 +0100

    dz09

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'youtube-api-dz09';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { YoutubeVideoi } from './components/youtube-videoi/youtube-videoi.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, YoutubeVideoi],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
